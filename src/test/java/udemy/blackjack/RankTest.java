0b4430d95af6b7a0c9b386a692356f808ee41440
EELMMAD <elmiramadadi@gmail.com>
2022-08-22 21:31:01 +0200

    write test for getValue

---
+++ src/test/java/udemy/blackjack/RankTest.java
@@ -0,0 +1,13 @@
+package udemy.blackjack;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RankTest {
+    /*
+    @Test
+    void getValueOfKing() {
+        assertEquals(10, Rank.KING.getValue());
+    }
+     */
+}
