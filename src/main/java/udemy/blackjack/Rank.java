17ef7f94732d07b67b907960881807c3fd94475c
EELMMAD <elmiramadadi@gmail.com>
2022-08-22 21:28:08 +0200

    add getValue method

--- src/main/java/udemy/blackjack/Rank.java
+++ src/main/java/udemy/blackjack/Rank.java
@@ -14,4 +14,17 @@ public enum Rank {
     JACK,
     QUEEN,
     KING;
+
+/*
+    //Rank KING = new Rank("KING");
+    //Rank QUEEN = new Rank("QUEEN");
+    //Rank JACK = new Rank("JACK");
+    //assertEquals(10, Rank.KING.getValue());
+
+    public int getValue() {
+      return   switch (this){
+            case JACK, QUEEN, KING -> 10;
+          default -> ordinal() + 1;
+        };
+ */
 }
